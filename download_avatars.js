6b85e0c1206044c90eb6ccb8eeec38a75eb99596
Thomas Saunders <thomas130@fastmail.com>
2018-03-19 21:53:25 +0000

    got urls

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,24 @@
+require('dotenv').config()
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'saunderst',
+      'Authorization': 'token ' + process.env.GH_AUTH
+    }
+  };
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+  var contribs = JSON.parse(result);
+  contribs.forEach(contrib => {
+    console.log (contrib.avatar_url);
+  });
 });
